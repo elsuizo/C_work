c80fbccb62a7274d9048240165758a9a345885a8
elsuizo <martin.noblia@openmailbox.org>
2016-11-28 16:49:39 -0300

    update

--- read_txt_files.c
+++ read_txt_files.c
@@ -29,20 +29,19 @@ You should have received a copy of the GNU General Public License
 
 const int STEPSIZE = 100;
 
-char ** load_file(char *filename);
+char** load_file(char* filename);
 
-int main(int argc, char *argv[])
-{
-    if(argc == 1)
-    {
+int main(int argc, char* argv[]){
+    if(argc == 1){
         fprintf(stderr, "Must supply a filename to read\n");
         exit(1);
     }
     // load file into data structure
-    char **words = load_file(argv[1]);
-    
-    for(int j = 0;words[j] != NULL; j++)
-        printf("%s\n", words[j]);
+    char** words = load_file(argv[1]);
+
+    for(int j = 0; words[j] != NULL; j++){
+       printf("%s\n", words[j]);
+    }
 
     printf("Done!!!\n");
 
@@ -50,31 +49,26 @@ int main(int argc, char *argv[])
 }
 
 
-char **load_file(char *filename)
-{
-    FILE *f = fopen(filename, "r");
+char** load_file(char* filename){
+    FILE* f = fopen(filename, "r");
     // check the read file status
-    if(!f)
-    {
+    if(!f){
         fprintf(stderr, "Cant open %s for reading\n", filename);
         return NULL;
     }
     int arrlen = 0;
-    // prealocate a chunk of memmory for the file the first 100  
+    // prealocate a chunk of memmory for the file the first 100
     //  NOTE(elsuizo:realloc con el primer parametro=NULL es lo mismo que malloc)-->Sun 01 May 2016 04:44:29 PM ART
-    char **lines = NULL; 
+    char** lines = NULL;
 
     char buffer[1000];
     int i = 0;
-    while(fgets(buffer, 1000, f))
-    {
-        if(i == arrlen)
-        {
+    while(fgets(buffer, 1000, f)){
+        if(i == arrlen){
             arrlen += STEPSIZE;
-            // the buffer is full and then reallocate the new space 
-            char **new_lines = realloc(lines, arrlen * sizeof(char *));
-            if(!new_lines)
-            {
+            // the buffer is full and then reallocate the new space
+            char** new_lines = realloc(lines, arrlen * sizeof(char *));
+            if(!new_lines){
                 fprintf(stderr, "Can't realloc men!!!\n");
                 exit(1);
             }
@@ -85,20 +79,17 @@ char **load_file(char *filename)
         // get the length of buffer
         int slen = strlen(buffer);
         // allocate space for the buffer
-        char *str = (char *)malloc((slen + 1) * sizeof(char));
+        char* str = (char*)malloc((slen + 1) * sizeof(char));
         // copy string from buffer to string
         strcpy(str, buffer);
         // attach str to the data structure
         lines[i] = str;
         i++;
-        
     }
-    
-    if(i == arrlen)
-    {
-        char **new_arr = realloc(lines, (arrlen + 1) * sizeof(char *));
-        if(!new_arr) 
-        {
+
+    if(i == arrlen){
+        char** new_arr = realloc(lines, (arrlen + 1) * sizeof(char*));
+        if(!new_arr){
             fprintf(stderr, "Can't realocate the fucking pipe");
             exit(1);
         }
