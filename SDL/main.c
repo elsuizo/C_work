cbdf748f678352aedb0284afa063205d18efb7dc
elsuizo <martin.noblia@openmailbox.org>
2016-12-01 11:43:47 -0300

    update

--- SDL/main.c
+++ SDL/main.c
@@ -6,7 +6,7 @@
 @email martin.noblia@openmailbox.org
 
 @brief
-
+SDL hola mundo
 @detail
 
 Licence:
@@ -25,10 +25,12 @@ You should have received a copy of the GNU General Public License
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define SCREEN_WIDTH  1280
 #define SCREEN_HEIGHT 720
 
+void render_screen(SDL_Renderer* renderer, SDL_Texture* screen);
 
 int main(void)
 {
@@ -38,7 +40,35 @@ int main(void)
          SDL_WINDOWPOS_UNDEFINED,
          SCREEN_WIDTH, SCREEN_HEIGHT,
          0);
+   SDL_Renderer* renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_SOFTWARE);
+   SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
 
+   SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+   SDL_Texture* screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+   bool done = false;
+   while (!done) {
+      SDL_Event event;
+      while (SDL_PollEvent(&event) != 0) {
+         if (event.type == SDL_QUIT) {
+            done = true;
+            break;
+         }
+      }
+      render_screen(renderer, screen);
+   }
+
+   SDL_DestroyRenderer(renderer);
+   SDL_DestroyWindow(window);
+   SDL_Quit();
    return 0;
 }
 
+void render_screen(SDL_Renderer* renderer, SDL_Texture* screen) {
+   uint32_t* pixels = calloc(SCREEN_WIDTH * SCREEN_HEIGHT, sizeof(uint32_t));
+   SDL_UpdateTexture(screen, NULL, pixels, SCREEN_WIDTH * sizeof(uint32_t));
+   SDL_RenderClear(renderer);
+   SDL_RenderCopy(renderer, screen, NULL, NULL);
+   SDL_RenderPresent(renderer);
+}
